b8a8f587eb85e2795d236b366ea39260e2f7b11f
Radha Pandey <pandeymuskan072@gmail.com>
2026-07-06 02:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -422,49 +422,35 @@ elif page == "Prediction":
                     xai_result["feature_importance"]
 
                 )
-# ------------------------------------------
+  # ------------------------------------------
 # AI REPORT
 # ------------------------------------------                
-                
-                
                 if predict_btn:
-
-    with st.spinner("Running AI Prediction..."):
-
-        try:
-
-            prediction_result = prediction_agent.predict(
-                latitude=latitude,
-                longitude=longitude
-            )
-
-            st.session_state.prediction = prediction_result
-
-            xai_result = xai_agent.explain(
-                prediction_result["Scaled_Data"],
-                prediction_agent.required_features
-            )
-
-            st.session_state.feature_importance = (
-                xai_result["feature_importance"]
-            )
-
-            report = report_agent.generate_report(
-                prediction_result,
-                st.session_state.feature_importance
-            )
-
-            st.session_state.report = report
-
-            st.success("Prediction Completed Successfully!")
-
-        except Exception as e:
-
-            import traceback
-
-            st.exception(e)
-            st.code(traceback.format_exc())
-
+                    with st.spinner("Running AI Prediction..."):
+                        try:
+                            prediction_result = prediction_agent.predict(
+                                latitude=latitude,
+                                longitude=longitude
+                            )
+                            st.session_state.prediction = prediction_result
+                            xai_result = xai_agent.explain(
+                                prediction_result["Scaled_Data"],
+                                prediction_agent.required_features
+                            )
+                            st.session_state.feature_importance = (
+                                xai_result["feature_importance"]
+                            )
+                            report = report_agent.generate_report(
+                                prediction_result,
+                                st.session_state.feature_importance
+                            )
+                            st.session_state.report = report
+                            st.success("Prediction Completed Successfully!")
+                        except Exception as e:
+                            import traceback
+                            st.exception(e)
+                            st.code(traceback.format_exc())
+              
     # ======================================================
     # SHOW RESULTS
     # ======================================================
